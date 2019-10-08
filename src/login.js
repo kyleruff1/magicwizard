19179139f3dca83794f897082936666449d23fdc
developrfw <developrfw@gmail.com>
2019-10-07 21:28:14 -0600

    React-Google-Login

---
+++ src/login.js
@@ -0,0 +1,31 @@
+// npm install react-google-login
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
+// or
+import { GoogleLogin } from 'react-google-login';
+ 
+ 
+const responseGoogle = (response) => {
+  console.log(response);
+}
+ 
+ReactDOM.render(
+  <GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />,
+  document.getElementById('googleButton')
+);
+
+import { GoogleLogout } from 'react-google-login';
+<GoogleLogout
+  clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+  buttonText="Logout"
+  onLogoutSuccess={logout}
+>
+</GoogleLogout>
