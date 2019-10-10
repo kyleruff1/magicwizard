a84e652998c8f77d4138598851ef57019b2081ce
baysik <iambaysik@gmail.com>
2019-10-09 21:19:28 -0600

    barebones landing page created

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav class="navbar navbar-expand fixed-top">
+        <div class="container">
+          <ul class="nav navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                idk
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
