9d99cf119a1ddd11e4a0db6b056b868507b025ba
baysik <iambaysik@gmail.com>
2019-10-14 20:47:33 -0600

    more routes added

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -3,43 +3,33 @@ import React, { Component } from "react";
 import Carousel from "../components/Carousel";
 import Navbar from "../components/Navbar";
 
-// function Browse() {
-//   return (
-//     <div>
-//       <Navbar />
-//       <Carousel />
-
-//     </div>
-//   );
-// }
-
-// export default Browse;
-
 export default class Browse extends Component {
   state = {
-    value: ""
+    searchTerm: ""
   };
 
   handleChange = event => {
-    this.setState({ value: event.target.value });
-    console.log(this.state.value);
+    console.log(this.state.searchTerm);
+    this.setState({ searchTerm: event.target.value });
   };
 
   render() {
     return (
       <div>
         <Navbar />
-        <label>
-          Name:
-          <input
-            className="form-control form-control-lg"
-            type="text"
-            placeholder=".form-control-lg"
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
+        <div class="container">
+          <label>
+            Name:
+            <input
+              className="form-control form-control-lg"
+              type="text"
+              placeholder="Search for a card"
+              type="text"
+              value={this.state.searchTerm}
+              onChange={this.handleChange}
+            />
+          </label>
+        </div>
       </div>
     );
   }
