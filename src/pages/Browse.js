1789cd6c01d6335a3233986f2ce7692c0b775fb1
Niles Dyer-Hunter <NXDH927@outlook.com>
2019-10-16 20:08:29 -0600

    searchUpdate

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 // import React from "react";
+import Carousel from "../components/Carousel";
 import Navbar from "../components/Navbar";
+const mtg = require("mtgsdk");
 
 export default class Browse extends Component {
-  state = {
-    searchTerm: ""
-  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      searchTerm: ""
+    };
+  }
+
+  handleClick = event => {
+    event.preventDefault();
+    console.log("Submit Click")
+    mtg.card.all({ name: this.state.searchTerm, pageSize: 1 })
+    .on('data', card => {
+        console.log(card.name)
+        console.log(card.imageUrl)
+    })
+  }
 
   handleChange = event => {
     console.log(this.state.searchTerm);
@@ -16,20 +35,15 @@ export default class Browse extends Component {
     return (
       <div>
         <Navbar />
-        <div class="container">
+        <div className="container">
           <label>
             Name:
-            <input
-              className="form-control form-control-lg"
-              type="text"
-              placeholder="Search for a card"
-              type="text"
-              value={this.state.searchTerm}
-              onChange={this.handleChange}
-            />
+            <input className="form-control form-control-lg" type="text" placeholder="Search for a card" type="text" value={this.state.searchTerm} onChange={this.handleChange}/>
           </label>
+          <button type="button" class="btn btn-light" onClick={this.handleClick}>Search</button>
         </div>
       </div>
     );
   }
 }
+
