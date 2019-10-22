26ee515cf9a93bf9344d0c2f7e840c1adfacb6a1
Kyle Ruff <kyleruff@gmail.com>
2019-10-21 21:41:35 -0600

    recovered new src

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,32 @@ import Carousel from "../components/Carousel";
 import Navbar from "../components/Navbar";
 import DeckList from "../components/DeckList";
 
+// TODO - add proptypes
+
+// const Home = props => {
+// 	if (props.user) {
+// 		return (
+// 			<div className="Home">
+// 				<p>Current User:</p>
+// 				<code>
+// 					{JSON.stringify(props)}
+// 				</code>
+// 			</div>
+// 		)
+// 	} else {
+// 		return (
+// 			<div className="Home">
+// 				<p>Current User:</p>
+// 				<code>
+// 					{JSON.stringify(props)}
+// 				</code>
+// 			</div>
+// 		)
+// 	}
+// }
+
+
+
 class Home extends Component {
   // state = {
   //   books: [],
